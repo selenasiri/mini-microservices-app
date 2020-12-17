2e9b5a30558e1460884e559c61da01b2fd98c783
Selena Siri <selenasiri@gmail.com>
2020-12-16 21:19:10 -0500

    posts index.js

--- posts/index.js
+++ posts/index.js
@@ -8,8 +8,15 @@ app.use(express.json())
 
 app.use(cors())
 
-const PORT = 4000
+const posts = { name: 'selena siri' };
 
+// https://posts.com/posts
+// http://localhost:4000/posts
+app.get('/posts', (req, res) => {
+  res.send(posts);
+})
+
+const PORT = 4000
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
 })
